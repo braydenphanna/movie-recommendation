a84e024bd5cff92a43736c2b4ba7d269d6f989e4
Brayden Hanna <Bhanna0608@gmail.com>
2026-04-15 17:00:21 -0400

    Add credits/imdb to info, clean up css, lower recommendations from 20->15

--- api.py
+++ api.py
@@ -2,15 +2,18 @@ import requests
 import json
 from movie import Movie
 
-def search(query): 
+# Enter an api key here
+api_key=""
 
-    # Enter an api key here
-    api_key=""
+headers = {
+    "accept": "application/json",
+    "Authorization": "Bearer "+ api_key
+}
 
-    headers = {
-        "accept": "application/json",
-        "Authorization": "Bearer "+ api_key
-    }
+# Takes a TMDB ID and a number of results (up to 20)
+def get_recommendations(query, num): 
+    # make sure num isn't over 20
+    if(num>20): num=20
 
     movies=[]
 
@@ -31,7 +34,22 @@ def search(query):
     # Add 20 recommended movies to movies list at indeces 1-21
     with open("recommendations.json", "w") as file:
         json.dump(recommendations.json(), file, indent=4)
-    for n in range(20):
+    for n in range(num):
         movies.append(Movie.from_json(n))
 
     return movies
+
+# Gets more movie info for a specific movie (credits and imdb id)
+def get_more_info(query):
+    url = "https://api.themoviedb.org/3/movie/"+str(query)+"/credits?language=en-US"
+    credits = requests.get(url, headers=headers).json()
+    url = "https://api.themoviedb.org/3/movie/"+str(query)+"/external_ids"
+    ext_ids = requests.get(url, headers=headers).json()
+
+    extra_info = {
+        "director": [member for member in credits['crew'] if member['job'] == 'Director'][0]["name"],
+        "cast": [member['name'] for member in credits.get('cast', [])[:10]],
+        "imdb": "https://www.imdb.com/title/"+ext_ids['imdb_id']
+    }
+
+    return extra_info
--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request,jsonify
-import get_recommendations
+import api
 from movie import Movie
 
 app = Flask(__name__)
@@ -12,12 +12,11 @@ def index():
 
 @app.route('/recommendations/', methods=['GET', 'POST'])
 def recommend():
-    return render_template('recommendations.html', recommendations=get_recommendations.search(request.form['query']))
+    return render_template('recommendations.html', recommendations=api.get_recommendations(request.form['query'],15))
 
-@app.route('/movie')
-def movie():
-    movie = Movie()
-    return jsonify(movie.to_dict())
+@app.route('/moreinfo/<id>')
+def more_info(id):
+    return api.get_more_info(id)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,port=8000)
